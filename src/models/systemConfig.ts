2127953c2bb5570b63d594577247e3b831a8e4a2
dodo <dodo@dodos-Laptop.local>
2025-12-23 00:28:53 +0800

    fix

--- src/models/systemConfig.ts
+++ src/models/systemConfig.ts
@@ -1,12 +1,11 @@
 import mongoose, { Schema } from "mongoose";
-import { BaseSchema } from "./base";
+import { baseSchemaOptions } from "./base";
 
 const schema = new Schema({
-    ...BaseSchema,
     name: { type: String, required: true },
     key: { type: String, required: true, unique: true },
     value: { type: String, required: true },
     desc: { type: String, required: false },
-}, { timestamps: true });
+}, baseSchemaOptions);
 
 export const SystemConfig = mongoose.models.SystemConfig || mongoose.model("SystemConfig", schema);
